ac79d34749778d10acb615260baa30c264128c46
pritesh-78 <pritesh.mangukiya604@gmail.com>
2024-09-24 02:11:18 -0400

    bug fix

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
-          <Route path="/" element={<Navigate to="/" />} /> {/* Default route */}
+          <Route path="/" element={<Navigate to="/#" />} /> {/* Default route */}
         </Routes>
         <Footer />
       </div>
