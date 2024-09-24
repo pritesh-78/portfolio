d41fbbe952919b586a95163012817f4164bb2ee2
pritesh-78 <pritesh.mangukiya604@gmail.com>
2024-09-24 01:46:49 -0400

    fixing routes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,54 @@
+// import React, { useState, useEffect } from "react";
+// import Preloader from "../src/components/Pre";
+// import Navbar from "./components/Navbar";
+// import Home from "./components/Home/Home";
+// import About from "./components/About/About";
+// import Projects from "./components/Projects/Projects";
+// import Footer from "./components/Footer";
+// import Resume from "./components/Resume/ResumeNew";
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Routes,
+//   Navigate
+// } from "react-router-dom";
+// import ScrollToTop from "./components/ScrollToTop";
+// import "./style.css";
+// import "./App.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+// function App() {
+//   const [load, upadateLoad] = useState(true);
+
+//   useEffect(() => {
+//     const timer = setTimeout(() => {
+//       upadateLoad(false);
+//     }, 1200);
+
+//     return () => clearTimeout(timer);
+//   }, []);
+
+//   return (
+//     <Router>
+//       <Preloader load={load} />
+//       <div className="App" id={load ? "no-scroll" : "scroll"}>
+//         <Navbar />
+//         <ScrollToTop />
+//         <Routes>
+//           <Route path="/home" element={<Home />} />
+//           <Route path="/project" element={<Projects />} />
+//           <Route path="/about" element={<About />} />
+//           <Route path="/resume" element={<Resume />} />
+//         </Routes>
+//         <Footer />
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
+
+
 import React, { useState, useEffect } from "react";
 import Preloader from "../src/components/Pre";
 import Navbar from "./components/Navbar";
@@ -7,7 +58,7 @@ import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router, // Change made here
   Route,
   Routes,
   Navigate
@@ -18,11 +69,11 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [load, upadateLoad] = useState(true);
+  const [load, updateLoad] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      upadateLoad(false);
+      updateLoad(false);
     }, 1200);
 
     return () => clearTimeout(timer);
@@ -35,10 +86,11 @@ function App() {
         <Navbar />
         <ScrollToTop />
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/project" element={<Projects />} />
           <Route path="/about" element={<About />} />
           <Route path="/resume" element={<Resume />} />
+          <Route path="/" element={<Navigate to="/home" />} /> {/* Default route */}
         </Routes>
         <Footer />
       </div>
